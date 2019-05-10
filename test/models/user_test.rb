492e98c3e21c9be724e445de6d73550f64830cdf
Kosei Ide <koseiide@gmail.com>
2019-05-10 17:51:25 +0900

    assertを忘れていたので追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,10 +21,12 @@ class UserTest < ActiveSupport::TestCase
 
   test "name should not be too long" do
     @user.name = "a" * 51
+    assert_not @user.valid?
     
   end
 
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
   end
 end
