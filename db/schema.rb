45feeb7235fdd0d6ad4792e4acd8557b94884042
Kosei Ide <koseiide@gmail.com>
2019-05-13 15:58:53 +0900

    マイグレーションを行った

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 20190511145304) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
