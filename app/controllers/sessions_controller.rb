cd2f614ac7f2dedfa2eca7e04bf43057ac63c6b9
Kosei Ide <koseiide@gmail.com>
2019-05-14 15:05:05 +0900

    .nowに書き換え

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
     # ユーザーログイン後は、ユーザー情報のページにリダイレクト
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
     render 'new'
     end
   end
