391da32ebcfbe8d4fefdf1803f5adbdc46de748b
Kosei Ide <koseiide@gmail.com>
2019-05-14 15:41:25 +0900

    メモ化

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,7 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  # 現在ログイン中のユーザーを返す（いる場合）
+  # 現在ログイン中のユーザーを返す（いる場合）いない場合はnilが返る
   def current_user
     if session[:user_id]
       @current_user || = User.find_by(id: session[:user_id])
