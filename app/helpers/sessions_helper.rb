75d74a17125e22f96ed4761921fee2e257e3704d
Kosei Ide <koseiide@gmail.com>
2019-05-15 17:19:28 +0900

    typo fix

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,7 @@ module SessionsHelper
   # ユーザーのセッションを永続的にする
   def remember(user)
     user.remember
-    cookies.permanent.signed[user_id] = user.id
+    cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
@@ -32,13 +32,13 @@ module SessionsHelper
   # 永続的セッションを破棄する
   def forget(user)
     user.forget
-    cookies.delete(user:_id)
+    cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
 
   # 現在のユーザーをログアウトする
   def log_out
-    forfet(current_user)
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
