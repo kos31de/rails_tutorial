63c66b0a6a7119cad751d78e2924c92900a09094
Kosei Ide <koseiide@gmail.com>
2019-05-20 18:37:25 +0900

    fix typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   def index
-    @users = User.where(activated:true)paginate(page: params[:page])
+    @users = User.where(activated:true).paginate(page: params[:page])
   end
 
   def show
