bcc0d6b56f15006fd256f1c651904aa01bfa9ccb
Kosei Ide <koseiide@gmail.com>
2019-05-20 17:57:48 +0900

    fix typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -6,15 +7,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       if user.activated?
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
       else
-      message = "Account not activated"
-      message += "Check your email for the activation link"
-      flash[:warning] = message
-      redirect_to root_url
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
       end
+    else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
