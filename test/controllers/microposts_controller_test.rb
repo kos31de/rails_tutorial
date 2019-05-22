b45955833d9ae85254339f867e4adf4b1afd4342
Kosei Ide <koseiide@gmail.com>
2019-05-22 17:28:49 +0900

    fix typo

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
-class MicropostsControllerTest < ApplicationController
+class MicropostsControllerTest < ActionDispatch::IntegrationTest
+
   def setup
     @micropost = microposts(:orange)
   end
@@ -13,7 +14,7 @@ class MicropostsControllerTest < ApplicationController
   end
 
   test "should redirect destroy when not logged in" do
-    assert_no_difference 'Microposy.count' do
+    assert_no_difference 'Micropost.count' do
       delete micropost_path(@micropost)
     end
     assert_redirected_to login_url
