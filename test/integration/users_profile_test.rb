9107d16a692128078f9fd7c33ac754ce71911040
Kosei Ide <koseiide@gmail.com>
2019-05-22 17:24:28 +0900

    :robot:

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -6,7 +6,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
-  
+
   test "profile display" do
     get user_path(@user)
     assert_template 'users/show'
