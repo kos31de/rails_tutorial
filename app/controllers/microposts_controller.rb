5ee00b1d0242d749d37e3250204ace53741c37b7
Kosei Ide <koseiide@gmail.com>
2019-05-22 17:02:02 +0900

    Micropostコントローラの認可テストを追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,21 @@
-class MicropostsController < ApplicationController
+require 'test_helper'
+
+class MicropostsControllerTest < ApplicationController
+  def setup
+    @micropost = microposts(:orange)
+  end
+
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post micropost_path, params: { micropost: { content: "Lorem ipsum"} }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Microposy.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
+  end
 end
