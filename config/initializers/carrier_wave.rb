a9d554b35c0a50898879bf4ce726c713d9f6b46b
Kosei Ide <koseiide@gmail.com>
2019-05-24 15:41:36 +0900

    設定を変更

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,10 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Amazon S3用の設定
@@ -9,5 +14,8 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['+0ZuG5FdO6JXz4i35cHH1+P++1iWkU4P6CLeqUsn']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
